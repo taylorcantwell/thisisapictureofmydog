a687c99569e342514fce9ca54e4260602e5e6689
Taylor Cantwell <taylor@birchal.com>
2024-05-03 09:58:11 +1000

    Update image source in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Image src="/logo.png" width={200} height={200} alt="my-dog" />
+      <Image src="/dog.JPG" width={200} height={200} alt="my-dog" />
     </main>
   );
 }
