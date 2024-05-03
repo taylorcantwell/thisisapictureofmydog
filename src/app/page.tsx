15123d282a8521790f943cfc02d5c00a25b97ad0
Taylor Cantwell <taylor@birchal.com>
2024-05-03 10:13:56 +1000

    Update image size in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Image src="/dog.JPG" width={200} height={200} alt="my-dog" />
+      <Image src="/dog.JPG" width={1170 / 2} height={1462 / 2} alt="my-dog" />
     </main>
   );
 }
